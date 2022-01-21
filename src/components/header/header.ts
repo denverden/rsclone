c8c460254f9b61a0f77df4bb35cd726a6145d3a4
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-01-21 22:02:43 +0300

    feat: header

---
+++ src/components/header/header.ts
@@ -0,0 +1,36 @@
+import Component from '../Component';
+import './header.scss';
+import logo from './logo.svg';
+
+const header = new Component({
+  selector: 'body',
+  template: `<header class="header">
+  <div class="header__container">
+    <div class="header-logo">
+      <a href="/" class="logo">
+        <img src="${logo}" alt="logo">
+      </a>
+      <nav class="menu">
+          <ul class="menu__list">
+            <li class="menu__item">
+              <a href="#" class="menu__link">об игре</a>
+            </li>
+            <li class="menu__item">
+              <a href="#" class="menu__link">гараж</a>
+            </li>
+            <li class="menu__item">
+              <a href="#" class="menu__link">типы гонок</a>
+            </li>
+            <li class="menu__item">
+              <a href="#" class="menu__link">рейтинг</a>
+            </li
+          </ul>
+      </nav>
+      <div class="user"></div>
+
+    </div>
+  </div>
+</header>`,
+});
+
+export default header;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import App from './components/app';
 import routes from './components/routing/routesData';
+import header from './components/header/header';
 
 import './global.scss';
 
 const APP = new App({
-  components: [],
+  components: [header],
   routes,
 });
 

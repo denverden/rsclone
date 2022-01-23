03d33054d989443cc1f76f4a3abf15cb10bea3f5
Rusweld <sanekoir2015@gmail.com>
2022-01-23 08:36:35 +0300

    feat: resolve pr problems

--- src/components/routing/routesData.ts
+++ src/components/routing/routesData.ts
@@ -1,9 +1,10 @@
 import error404 from '../error404/error404';
 import { IRoute } from '../../interface/IRoute';
-import footer from '../footer/footer'
+import footer from '../footer/footer';
+
 
 const routes: Array<IRoute> = [
-  { path: 'footer', components: [footer] },
+  { path: '', components: [footer] },
   { path: '***', components: [error404] },
 ];
 

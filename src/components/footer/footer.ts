0d5f7b110f355224eb70b4327d872a4d637591f9
Rusweld <sanekoir2015@gmail.com>
2022-01-24 14:48:45 +0300

    feat: add links

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -8,8 +8,13 @@ const footer = new Component({
         <footer class="footer">
             <div class="wrapper">
                 <div class="footer-content">
-                    <div class="footer-content__info">&#169; 2022 KEYBOARD RACE | Разработчики: Henadzi, Denverden, Rusveld</div>
-                    <div class="footer-content__logo"></div>
+                    <div class="footer-content__info">
+                    &#169; 2022 KEYBOARD RACE | Разработчики:
+                     <a class = "footer-content__info-link" target = "_blank" href = "https://github.com/oreshkevich">Henadzi</a>, 
+                     <a class = "footer-content__info-link" target = "_blank" href = "https://github.com/denverden">Denverden</a>, 
+                     <a class = "footer-content__info-link" target = "_blank" href = "https://github.com/Rusveld">Rusveld</a>
+                    </div>
+                    <a href = "https://rs.school/" target = "_blank" class="footer-content__logo"></a>
                 </div>
             </div>
         </footer>

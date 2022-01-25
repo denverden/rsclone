15055e3de9aa0e0c058e7de8b57752028d4d445c
Denis Novikov <denverden@mail.ru>
2022-01-25 19:22:57 +0300

    28 feat: add methods for working with templates

--- src/components/error404/error404.ts
+++ src/components/error404/error404.ts
@@ -2,7 +2,7 @@ import Component from '../Component';
 
 const error404 = new Component({
   selector: 'body',
-  template: `<h1>Страница не найдена.</h1>`,
+  template: `<h1><Страница не найдена.></h1>`,
 });
 
 export default error404;
--- src/interface/IData.ts
+++ src/interface/IData.ts
@@ -1,4 +1,5 @@
 export interface IData {
   selector: string;
   template: string;
+  elem?: HTMLElement;
 }

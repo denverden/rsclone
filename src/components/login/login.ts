11016d00b3886d33f2e9df2d72b7445a45ac936b
Rusweld <sanekoir2015@gmail.com>
2022-01-26 04:43:44 +0300

    add: login scss

--- src/components/login/login.ts
+++ src/components/login/login.ts
@@ -9,16 +9,18 @@ const login = new Component({
         <button class="login-btn">Login</button>
         <button class="registration-btn">Registration</button>
       </div>
-      <div class="login-inputs">
-        <input type="text" id="login-username">
-        <input type="password" id="login-username">
-        <button class="login-putter">Login</button>
-      </div>
-      <div class="registration-inputs">
-        <input type="text" id="registration-username">
-        <input type="password" id="registration-password--1" >
-        <input type="password" id="registration-password--2">
-        <button class="registration-putter">Registration</button>
+      <div class="inputs">
+        <div class="login-inputs">
+          <input placeholder="login" type="text" id="login-username">
+          <input placeholder="password" type="password" id="login-username">
+          <button class="login-putter">Login</button>
+        </div>
+        <div class="registration-inputs">
+          <input placeholder="login" type="text" id="registration-username">
+          <input placeholder="password" type="password" id="registration-password--1" >
+          <input placeholder="confirm password" type="password" id="registration-password--2">
+          <button class="registration-putter">Registration</button>
+        </div>
       </div>
     </div>
   `

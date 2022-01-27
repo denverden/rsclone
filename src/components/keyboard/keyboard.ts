1219066c2c7cd472cc14639c0ff9a04ba076b42a
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-01-27 11:00:54 +0300

    feat: add keyboard

---
+++ src/components/keyboard/keyboard.ts
@@ -0,0 +1,235 @@
+import { IData } from '../../interface/IData';
+import { IKeyLayout } from '../../interface/IKeyLayout';
+import { IText } from '../../interface/IText';
+import HTTP from '../http';
+import Component from '../Component';
+import './keyboard.scss';
+
+class Keyboard extends Component {
+  private keysContainer: HTMLElement;
+  private lang: string;
+
+  constructor(data: IData) {
+    super(data);
+    this.lang = 'ru';
+    this.keysContainer = null;
+    this.stateTemplate = { text: 'loading...' };
+  }
+
+  init() {
+    this.keysContainer = document.createElement('div');
+    this.keysContainer.classList.add('keyboard__keys');
+    this.keysContainer.appendChild(this.createKeys());
+    this.elem.querySelector('#keyboard').appendChild(this.keysContainer);
+  }
+
+  createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keyLayout: IKeyLayout = {
+      Backquote: '`',
+      Digit1: '1',
+      Digit2: '2',
+      Digit3: '3',
+      Digit4: '4',
+      Digit5: '5',
+      Digit6: '6',
+      Digit7: '7',
+      Digit8: '8',
+      Digit9: '9',
+      Digit0: '0',
+      Minus: '-',
+      Equal: '=',
+      Backspace: '⇦',
+      KeyQ: 'Q',
+      KeyW: 'W',
+      KeyE: 'E',
+      KeyR: 'R',
+      KeyT: 'T',
+      KeyY: 'Y',
+      KeyU: 'U',
+      KeyI: 'I',
+      KeyO: 'O',
+      KeyP: 'P',
+      BracketLeft: '[',
+      BracketRight: ']',
+      CapsLock: 'caps',
+      KeyA: 'A',
+      KeyS: 'S',
+      KeyD: 'D',
+      KeyF: 'F',
+      KeyG: 'G',
+      KeyH: 'H',
+      KeyJ: 'J',
+      KeyK: 'K',
+      KeyL: 'L',
+      Semicolon: ';',
+      Quote: "'",
+      Enter: 'enter',
+      ShiftLeft: 'shift',
+      KeyZ: 'Z',
+      KeyX: 'X',
+      KeyC: 'C',
+      KeyV: 'V',
+      KeyB: 'B',
+      KeyN: 'N',
+      KeyM: 'M',
+      Comma: ',',
+      Period: '.',
+      Slash: '?',
+      ShiftRight: 'shift',
+      Space: 'space',
+    };
+    const keyLayoutRu: IKeyLayout = {
+      Backquote: 'Ё',
+      Digit1: '1',
+      Digit2: '2',
+      Digit3: '3',
+      Digit4: '4',
+      Digit5: '5',
+      Digit6: '6',
+      Digit7: '7',
+      Digit8: '8',
+      Digit9: '9',
+      Digit0: '0',
+      Minus: '-',
+      Equal: '=',
+      Backspace: '⇦',
+      KeyQ: 'Й',
+      KeyW: 'Ц',
+      KeyE: 'У',
+      KeyR: 'К',
+      KeyT: 'Е',
+      KeyY: 'Н',
+      KeyU: 'Г',
+      KeyI: 'Ш',
+      KeyO: 'Щ',
+      KeyP: 'З',
+      BracketLeft: 'Х',
+      BracketRight: 'Ъ',
+      CapsLock: 'caps',
+      KeyA: 'Ф',
+      KeyS: 'Ы',
+      KeyD: 'В',
+      KeyF: 'А',
+      KeyG: 'П',
+      KeyH: 'Р',
+      KeyJ: 'О',
+      KeyK: 'Л',
+      KeyL: 'Д',
+      Semicolon: 'Ж',
+      Quote: 'Э',
+      Enter: 'enter',
+      ShiftLeft: 'shift',
+      KeyZ: 'Я',
+      KeyX: 'Ч',
+      KeyC: 'С',
+      KeyV: 'М',
+      KeyB: 'И',
+      KeyN: 'Т',
+      KeyM: 'Ь',
+      Comma: 'Б',
+      Period: 'Ю',
+      Slash: '.',
+      ShiftRight: 'shift',
+      Space: 'space',
+    };
+
+    const keys = Object.keys(keyLayout);
+    keys.forEach((key) => {
+      const keyElement = document.createElement('button');
+      const insertLineBreak = ['Backspace', 'BracketRight', 'Enter'].indexOf(key) !== -1;
+      keyElement.setAttribute('type', 'button');
+      keyElement.classList.add('keyboard__key');
+      if (this.lang === 'ru') {
+        keyElement.textContent = keyLayoutRu[key];
+        keyElement.setAttribute('data', keyLayoutRu[key]);
+      } else {
+        keyElement.textContent = keyLayout[key];
+        keyElement.setAttribute('data', keyLayout[key]);
+      }
+
+      fragment.appendChild(keyElement);
+      if (insertLineBreak) {
+        fragment.appendChild(document.createElement('br'));
+      }
+    });
+
+    return fragment;
+  }
+
+  eventBtn() {
+    const showColor = document.querySelector('.show-color');
+    showColor.addEventListener('click', (event) => {
+      showColor.classList.toggle('hide-color');
+      document.querySelectorAll('.keyboard__key').forEach((elem) => {
+        elem.classList.toggle('keyboard__key--coloring');
+      });
+    });
+
+    const showKeyboard = document.querySelector('.show-keyboard');
+    const showHand = document.querySelector('.show-hand');
+    const rightHandOne = document.querySelector('.right-hand-one');
+
+    showKeyboard.addEventListener('click', (event) => {
+      showKeyboard.classList.toggle('hide-keyboard');
+      this.elem.querySelector('#keyboard').classList.toggle('keyboard--hidden');
+    });
+
+    showHand.addEventListener('click', (event) => {
+      showHand.classList.toggle('hide-hand');
+      rightHandOne.classList.toggle('active--hand');
+    });
+
+    setTimeout(function () {
+      const textAll = document.querySelector('.text').innerHTML;
+
+      let resultArr = textAll.split('');
+      let result = resultArr[0];
+      console.log(result);
+
+      const btn = document.querySelector('.keyboard__key[data="' + resultArr[0] + '"]') as HTMLElement;
+      btn.style.backgroundColor = 'red';
+      document.addEventListener('keydown', (event) => {
+     
+        let i = 0;
+        if (event.key === result) {
+          btn.style.backgroundColor = '';
+          console.log(resultArr[i + 1]);
+          const btn2 = document.querySelector('.keyboard__key[data="' + resultArr[i + 1].toUpperCase() + '"]') as HTMLElement;
+          btn2.style.backgroundColor = 'red';
+        }
+      });
+    }, 3000);
+  }
+
+  async loadText() {
+    const txt = await HTTP.getText<IText>();
+    this.stateTemplate.text = txt.info.text;
+    this.elem.querySelector('.text').innerHTML = txt.info.text;
+    this.lang = txt.info.lang;
+    console.log(this.lang);
+  }
+
+  afterRender() {
+    this.init();
+    this.eventBtn();
+    this.loadText();
+  }
+}
+
+const keyboard = new Keyboard({
+  selector: '.page__main',
+  template: `
+  <div class="text">{{ text }}</div>
+  <div class="container-keyboard">
+  <div id="hands">
+    <div class="hand right-hand-one" id="rhand1"></div>
+  </div>
+  <span class="show-keyboard" title="Скрыть клавиатуру"></span>
+  <span class="show-color" title="Показать цвет"></span>
+  <span class="show-hand" title="Показать руки"></span>
+  <div id="keyboard"></div>
+</div>`,
+});
+
+export default keyboard;
--- src/components/routing/routesData.ts
+++ src/components/routing/routesData.ts
@@ -1,8 +1,10 @@
 import error404 from '../error404/error404';
 import { IRoute } from '../../interface/IRoute';
+import keyboard from '../keyboard/keyboard';
 
 const routes: Array<IRoute> = [
   { path: '', components: [] },
+  { path: 'learn', components: [keyboard] },
   { path: '***', components: [error404] },
 ];
 

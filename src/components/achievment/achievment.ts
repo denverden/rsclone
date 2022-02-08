2c390ed2b5352b2eecb3bf41347cbdea25ff16ec
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-02-08 22:13:50 +0300

    feat: add achievement

--- src/components/achievment/achievment.ts
+++ src/components/achievment/achievment.ts
@@ -1,14 +1,27 @@
-import Component from "../component";
-import './achievment.scss'
+import Component from '../component';
+import './achievment.scss';
+import wreath from './img/wreath.svg';
 
 const achievment = new Component({
   selector: `.page__main`,
   template: `<div class="parallax-bg achievment-parallax"></div>
-            <div class="main-acc-container achievment-container">
+              <div class="main-acc-container achievment-container">
+                <h2 class="main__title">Ваши достижения </h2>
+                <div class="achievement">
+                    <img class="achievement__img-item" src="${wreath}" alt="road">
+                    <img class="achievement__img-item" src="${wreath}" alt="road">
+                    <img class="achievement__img-item" src="${wreath}" alt="road">
+                    <img class="achievement__img-item" src="${wreath}" alt="road">
+                    <img class="achievement__img-item" src="${wreath}" alt="road">
+                    <img class="achievement__img-item" src="${wreath}" alt="road">
+                    <img class="achievement__img-item" src="${wreath}" alt="road">
+                    <img class="achievement__img-item" src="${wreath}" alt="road">
+                    <img class="achievement__img-item" src="${wreath}" alt="road">
+                </div>
+              </div>
             </div>
 
-`
+`,
 });
-
 
 export default achievment;
---
+++ src/interface/IAchievements.ts
@@ -0,0 +1,7 @@
+export interface IAchievements {
+  _id: string;
+  name: string;
+  description: string;
+  image: string;
+  sort: number;
+}

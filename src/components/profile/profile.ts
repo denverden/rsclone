d0faa7ca9f48e408d190b2cab5311fd0ba53581d
Rusweld <sanekoir2015@gmail.com>
2022-02-08 15:56:38 +0300

    feat: start work in profile

--- src/components/profile/profile.ts
+++ src/components/profile/profile.ts
@@ -2,9 +2,40 @@ import Component from '../component';
 
 const profile = new Component({
   selector: '.page__main',
-  template: '<h1>Profile</h1>',
+  template: `<div class="parallax-bg">
+  <div class="personal-stats">
+    <div class="stats-card">
+      <div class="card-img1"></div>
+      <div class="card-info">
+        <p class="achive-value"><span>0</span> зн/мин</p>
+        <p class="achive-name">Макс. скорость</p>
+      </div>
+    </div>
+    <div class="stats-card">
+      <div class="card-img1"></div>
+      <div class="card-info">
+        <p class="achive-value"><span>0</span> зн/мин</p>
+        <p class="achive-name">Макс. скорость</p>
+      </div>
+    </div>
+    <div class="stats-card">
+      <div class="card-img1"></div>
+      <div class="card-info">
+        <p class="achive-value"><span>0</span> зн/мин</p>
+        <p class="achive-name">Макс. скорость</p>
+      </div>
+    </div>
+    <div class="stats-card">
+      <div class="card-img1"></div>
+      <div class="card-info">
+        <p class="achive-value"><span>0</span> зн/мин</p>
+        <p class="achive-name">Макс. скорость</p>
+      </div>
+    </div>
+  </div>
+</div>
+`
 });
 
 export default profile;
-
 

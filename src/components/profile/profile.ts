35917f95e616c40fcf5ac22106a830b4357ff0f6
Rusweld <sanekoir2015@gmail.com>
2022-02-09 19:40:24 +0300

    fix: footer position bug

--- src/components/profile/profile.ts
+++ src/components/profile/profile.ts
@@ -3,6 +3,31 @@ import './profile.scss'
 import appStore from '../appStore';
 
 class Profile extends Component{
+  beforeRender() {
+    this.stateTemplate = {
+      races: appStore.user.races.toString(),
+      signs: appStore.user.signs.toString(),
+      time: this.formatGameDate(appStore.user.time),
+      mistakes: this.percentMistakes(appStore.user.mistakes),
+      username: appStore.user.username !== '' ? appStore.user.username : 'незнакомец',
+    };
+  }
+
+  formatGameDate(timeSeconds: number) {
+    const hours = new Date(timeSeconds * 1000).getUTCHours().toString();
+    const minutes = new Date(timeSeconds * 1000).getUTCMinutes().toString();
+    const seconds = new Date(timeSeconds * 1000).getUTCSeconds().toString();
+
+    return [hours, minutes, seconds].join(':');
+  }
+
+  percentMistakes(mistakes: number) {
+    const hundredPercent = 100;
+    const percent = appStore.user.signs === 0 ? '0' : ((mistakes * hundredPercent) / appStore.user.signs).toFixed(1);
+
+    return `${percent}%`;
+  }
+
   afterRender(): void {
       const name = document.querySelector('.personal-name')
       name.textContent = appStore.user.username
@@ -14,32 +39,22 @@ const profile = new Profile({
   selector: '.page__main',
   template: `<div class="parallax-bg">
               <div class="personal-stats">
-                <div class="stats-card">
-                  <div class="card-img1"></div>
-                  <div class="card-info">
-                    <p class="achive-value"><span>0</span> зн/мин</p>
-                    <p class="achive-name">Макс. скорость</p>
+                <div class="info-wrapper">
+                  <div class="info">
+                    <span class="info__value">{{ races }}</span>
+                    <span class="info__title">Заездов пройдено</span>
                   </div>
-                </div>
-                <div class="stats-card">
-                  <div class="card-img1"></div>
-                  <div class="card-info">
-                    <p class="achive-value"><span>0</span> зн/мин</p>
-                    <p class="achive-name">Макс. скорость</p>
+                  <div class="info">
+                    <span class="info__value">{{ signs }}</span>
+                    <span class="info__title">Знаков набрано</span>
                   </div>
-                </div>
-                <div class="stats-card">
-                  <div class="card-img1"></div>
-                  <div class="card-info">
-                    <p class="achive-value"><span>0</span> зн/мин</p>
-                    <p class="achive-name">Макс. скорость</p>
+                  <div class="info">
+                    <span class="info__value">{{ time }}</span>
+                    <span class="info__title">Время набора</span>
                   </div>
-                </div>
-                <div class="stats-card">
-                  <div class="card-img1"></div>
-                  <div class="card-info">
-                    <p class="achive-value"><span>0</span> зн/мин</p>
-                    <p class="achive-name">Макс. скорость</p>
+                  <div class="info">
+                    <span class="info__value">{{ mistakes }}</span>
+                    <span class="info__title">Процент ошибок</span>
                   </div>
                 </div>
               </div>

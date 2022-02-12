e9e0d41055737fa9e37ba0b431a42e99442f026f
Rusweld <sanekoir2015@gmail.com>
2022-02-12 23:19:58 +0300

    add: change avatar feature

--- src/components/profile/profile.ts
+++ src/components/profile/profile.ts
@@ -1,6 +1,45 @@
 import Component from '../component';
 import './profile.scss'
 import appStore from '../appStore';
+import http from '../http';
+
+const logExample = [{
+    iduser: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: Number,
+      default: 0,
+    },
+    type: {
+      type: String,
+      default: 'завершено',
+    },
+    text: {
+      type: String,
+      default: 'Заезд завершен',
+    },
+  },
+  {
+    iduser: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: Number,
+      default: 2345,
+    },
+    type: {
+      type: String,
+      default: 'не завршено',
+    },
+    text: {
+      type: String,
+      default: 'Урок не пройден',
+    },
+  },
+]
 
 class Profile extends Component{
   beforeRender() {
@@ -10,7 +49,9 @@ class Profile extends Component{
       time: this.formatGameDate(appStore.user.time),
       mistakes: this.percentMistakes(appStore.user.mistakes),
       username: appStore.user.username !== '' ? appStore.user.username : 'незнакомец',
+      avatar: appStore.user.avatar,
     };
+
   }
 
   formatGameDate(timeSeconds: number) {
@@ -29,8 +70,40 @@ class Profile extends Component{
   }
 
   afterRender(): void {
-      const name = document.querySelector('.personal-name')
-      name.textContent = appStore.user.username
+      const name = document.querySelector('.personal-name');
+      const photoInput = document.getElementById('photo-input') as HTMLInputElement;
+      const table = document.querySelector('.table-races')
+
+      name.textContent = appStore.user.username;
+
+
+
+      function handleFiles() {
+        const fileList = this.files;
+        const reader = new FileReader();
+
+        reader.readAsDataURL(fileList[0])
+        reader.onload = () =>{
+          appStore.user.avatar = `${reader.result}`
+          http.updateUser();
+        }
+      }
+      logExample.forEach((elem)=>{
+        photoInput.addEventListener('change', handleFiles)
+        const tableItem = document.createElement('div')
+        tableItem.classList.add('table-races__item')
+        const tableTime = document.createElement('div')
+        tableTime.classList.add('table-races__time')
+        tableTime.textContent = `${elem.time.default}`
+        const tableStatus = document.createElement('div')
+        tableStatus.classList.add('table-races__status')
+        tableStatus.textContent = `${elem.type.default}`
+        const tableText = document.createElement('div')
+        tableText.classList.add('table-races__text')
+        tableText.textContent = `${elem.text.default}`
+        tableItem.append(tableTime, tableStatus, tableText)
+        table.appendChild(tableItem)
+      })
   }
 }
 
@@ -62,17 +135,18 @@ const profile = new Profile({
             <div class="main-acc-container">
               <div class="personal-info">
                 <div class="personal-photo">
-                  <div class="photo-container"></div>
+                  <img class="photo-container" src="{{ avatar }}"></img>
+                  <input type="file" id="photo-input" class="hidden">
+                  <label for="photo-input" class="photo-changer">Изменить</label>
                 </div>
                 <p class="personal-name"></p>
               </div>
               <div class="table-races">
-                <div class="table-races__item">
-                  <div class="table-races__date">09.02.2020</div>
+                <!-- <div class="table-races__item">
                   <div class="table-races__time">19:00</div>
                   <div class="table-races__status">Завершен</div>
                   <div class="table-races__text">Завершен заезд</div>
-                </div>
+                </div> -->
               </div>
             </div>
 

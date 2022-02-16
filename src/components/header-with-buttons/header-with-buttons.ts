fdb2bd7efd3d7377d86da804979913036b865870
Rusweld <sanekoir2015@gmail.com>
2022-02-16 18:37:04 +0300

    fix: filename problem

--- src/components/header-with-buttons/header-with-buttons.ts
+++ src/components/header-with-buttons/header-with-buttons.ts
@@ -1,5 +1,5 @@
 import Component from '../component';
-import logo from './logo.svg';
+import logo from './logo-new.svg';
 import './header-with-buttons.scss'
 const headerWithButtons = new Component({
   selector: '.page__header',

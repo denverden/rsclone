aa793f49154d1900988334e86bf73e6df2cae3c1
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-02-18 20:18:42 +0300

    feat: img

--- src/components/race/race.ts
+++ src/components/race/race.ts
@@ -1,9 +1,24 @@
 import Component from '../component';
 import './race.scss';
+class Race extends Component {
+  raceImg() {
+    const images = ['snow.svg', 'night.svg', 'rain1.svg', 'street2.svg'];
+    const random = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
+    const min = 0;
+    const max = 3;
+    const index = random(min, max);
+    const block = document.getElementById('block');
+    block.style.backgroundImage = `url('./assets/images/${images[index]}')`;
+  }
 
-const race = new Component({
+  afterRender() {
+    this.raceImg();
+  }
+}
+
+const race = new Race({
   selector: '.race',
-  template: '<div class="race__street"></div><div class="race__road"></div>',
+  template: '<div id="block" class="race__street"></div><div class="race__road"></div>',
 });
 
 export default race;
